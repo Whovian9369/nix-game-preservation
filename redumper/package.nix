9233fa7ed0826c4dba2e32c33f89849d3f92e457
Whovian9369 <Whovian9369@gmail.com>
2025-02-15 11:09:37 -0500

    Update `redumper` and remove previously used patches.

--- redumper/package.nix
+++ redumper/package.nix
@@ -11,15 +11,15 @@
   # by default here.
 }:
 
-llvmPackages.libcxxStdenv.mkDerivation /* rec */ {
+llvmPackages.libcxxStdenv.mkDerivation {
   pname = "redumper";
-  version = "build_458";
+  version = "build_479";
 
   src = fetchFromGitHub {
     owner = "superg";
     repo = "redumper";
-    rev = "1e3f4c28395aaa744502ad9566eb23b8b92e60a0";
-    hash = "sha256-qpHSzFlSy+MRVnOUOS4SyKfx3y5KVvD5eqUDzNoeyxo=";
+    rev = "72f0a93e660884f4085aef69f321720d2e0d7138";
+    hash = "sha256-gXUj0pUbkiPrW3WE7QYIZ9nQQPvZecfpH9+jjbnna5g=";
   };
 
   nativeBuildInputs = [
@@ -28,27 +28,14 @@ llvmPackages.libcxxStdenv.mkDerivation /* rec */ {
     llvmPackages.clang-tools
   ];
 
-  patches = [
-    ./remove_compiler_warnings.diff
-      # Remove warning:
-      # clang++: warning: argument unused during compilation: '-stdlib=libc++' [-Wunused-command-line-argument]
-    ./build_timestamp.diff
-      # Change timestamp variables to be "CACHE" ones that can be overridden.
-      # This lets us force various date values to match `src.rev` date.
-  ];
-
-  # Originally based on
-  # https://github.com/superg/redumper/blob/main/.github/workflows/cmake.yml
   cmakeFlags = [
     (lib.cmakeBool "CMAKE_BUILD_WITH_INSTALL_RPATH" true)
-    (lib.cmakeFeature "REDUMPER_CLANG_LINK_OPTIONS" "")
-      # overrides the '-static' default
 
     # Fix version datestamp to date of `src.rev` commit.
-    # See notes for build_timestamp.diff patch
+    # It would be nice if I was able to automate this.
     (lib.cmakeFeature "REDUMPER_VERSION_MAJOR" "2025") # Year
-    (lib.cmakeFeature "REDUMPER_VERSION_MINOR" "01") # Month
-    (lib.cmakeFeature "REDUMPER_VERSION_PATCH" "31") # Day
+    (lib.cmakeFeature "REDUMPER_VERSION_MINOR" "02") # Month
+    (lib.cmakeFeature "REDUMPER_VERSION_PATCH" "09") # Day
     (lib.cmakeFeature "REDUMPER_VERSION_BUILD" "NixOS")
   ];
 
--- redumper/release.nix
+++ redumper/release.nix
@@ -6,12 +6,12 @@
 
 stdenvNoCC.mkDerivation rec {
   name = "redumper-bin-v${version}";
-  version = "458";
-  date = "2025.01.31";
+  version = "479";
+  date = "2025.02.10";
 
   src = fetchzip {
-    url = "https://github.com/superg/redumper/releases/download/build_${version}/redumper-${date}_build${version}-Linux.zip";
-    hash = "sha256-TfChRXzSJDQw5BUq/BrVAyWfCvw2LcNiswjfS6IXKcQ=";
+    url = "https://github.com/superg/redumper/releases/download/build_${version}/redumper-${date}_build${version}-Linux64.zip";
+    hash = "sha256-SGCGTpW8nUn2fVRHMlD5NsrWukiHtED3EiCjQXOQk3c=";
   };
 
   installPhase = ''
